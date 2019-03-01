6a92f795351817307aa83a5db820daa23ae52bfd
BrandonSaravia <BPSaravia@gmail.com>
2019-03-01 13:56:34 -0500

    i answered all array and advanced array method questions creating an array that ... a new array called universities that contains all the univeristies in the graduates array. ...a new array called contactInfo that contains both first name and email of each student. ...Find out how many universities have the string Uni included in their name. Create a new array called uni that contains them all. Log the result. ...display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it. ...a list of all their animal's names (names only, not scientific) converted to lower case. ... Find out which animals have a population less than 5. ...Find the total population from all the zoos using the .reduce() method. in objects-array.js file.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -79,7 +79,15 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
+
+for(let i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university);
+}
+
+universities.sort();
+
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -89,11 +97,23 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for(let i = 0; i < graduates.length; i++){
+  contactInfo.push(graduates[i].first_name + " " + graduates[i].email);
+}
+
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+
+for(let i = 0; i < graduates.length; i++){
+    if(graduates[i].university.includes('Uni')){
+      uni.push(graduates[i].university)
+    }
+}
+
 console.log(uni);
 
 
@@ -119,6 +139,11 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+
+zooAnimals.forEach(function(currentValue){
+  animalNames.push(currentValue.animal_name +' '+ currentValue.scientific_name);
+})
+
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -128,6 +153,11 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 */
 
 const lowerCase = [];
+
+const namesLowerCase = zooAnimals.map( function(currentValue){ 
+    lowerCase.push(currentValue.animal_name.toLowerCase());
+  });
+
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -136,6 +166,13 @@ The zoos are concenred about animals with a lower population count. Find out whi
 
 */
 const largerPopulation = [];
+
+for(let i = 0; i < zooAnimals.length; i++){
+  if(zooAnimals[i].population <= 5){
+    largerPopulation.push(zooAnimals[i])
+  }
+}
+
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -143,7 +180,9 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce(function(accumilator,currentValue){
+  return accumilator + currentValue.population;}, 0);
+
 console.log(populationTotal);
 
 
